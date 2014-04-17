813d1507ee09cfdba8f6cf2bba65ccf8ba51fd26
tigarcia <tim.m.garcia@gmail.com>
2014-04-17 15:06:31 -0700

    Making some small changes to how rspec is configured

--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -1,9 +1,6 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "Sinatra Movie Application With OMDB API" do
-  def app
-    Sinatra::Application
-  end
 
   describe "Home page" do
     it "should allow accessing the home page" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ require 'rspec'
 require 'rack/test'
 require File.expand_path '../../movies.rb', __FILE__
 
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() Sinatra::Application end
+end
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
@@ -13,7 +18,7 @@ RSpec.configure do |config|
   config.filter_run :focus
 
   config.include Rack::Test::Methods
-
+  config.include RSpecMixin
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
