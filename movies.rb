f5cf9a16c056b2b420f4c4ba8b2ecf8ba25effcd
spencereldred <eldredspencer@gmail.com>
2014-04-17 15:28:47 -0700

    Revise movies.rb file

--- movies.rb
+++ movies.rb
@@ -2,16 +2,47 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
-require 'pry'
 
-# A setup step to get rspec tests running.
 configure do
   root = File.expand_path(File.dirname(__FILE__))
-  set :views, File.join(root,'views')
+  set :views, File.join(root, 'views')
 end
+
 
 get '/' do
-  
+# redirect to form to get movie
+
+  @method = "get"
+  @action   = "/movie"
+
+  erb :form
 end
 
+get '/movie' do
 
+request = Typhoeus.get(
+  "http://www.omdbapi.com",
+  :params => { :s => params[:query] }
+  )
+
+  @movies = JSON.parse(request.body)["Search"]
+
+  puts "@@@@@@@@@@@@@@@@ movies = #{@movies}"
+
+  erb :index
+end
+
+get "/movie/:id" do
+  # show a particular movie
+  @id = params[:captures][0]
+
+  request = Typhoeus.get(
+    "http://www.omdbapi.com",
+    :params => { :i => @id }
+    )
+
+  @movie_info = JSON.parse(request.body)
+  #raise @movie_info.inspect
+
+  erb :movie_view
+end
