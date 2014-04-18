f9077e6d94d68f50dd529b09ad51eb961ce5dfb8
blairprophett <blaircuny@gmail.com>
2014-04-17 22:21:49 -0700

    working links in a list

--- movies.rb
+++ movies.rb
@@ -21,19 +21,12 @@ get '/results' do
 	results= Typhoeus.get("www.omdbapi.com/?s=#{search}")
 	ombd_data = JSON.parse(results.body)
 	# ombd_data.inspect
-erb :index
-@movies=ombd_data.to_s
 
+	@movies=ombd_data["Search"]
 
-# if ombd_data["Search"] == nil
-# 	 "<h1>Sorry, we could not find that title. Try again.</h1>"
-# else
-# 	ombd_data["Search"].each do |h|
-# 	# "<a href='/poster/#{h["imdbID"]}'><li>#{h["Title"]} - #{h["Year"]}<br></br></li></a>"
-# 	end
-# end
-
+	erb :index
 end
+
 
 get '/poster/:imdbID' do
 	search = params[:imbdID]
