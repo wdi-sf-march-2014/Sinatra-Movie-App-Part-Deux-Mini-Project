9314b3e291d83c1a319daee7412e3ff4c076b978
Henry Huang <hhuang2088@gmail.com>
2014-04-18 19:21:52 -0700

    functionality finished

--- app.rb
+++ app.rb
@@ -1,24 +1,45 @@
-require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
-require 'erb'
+require 'pry'
 
 get '/' do
+	@method = "get"
+	@action = "/show"
 	erb :index
 end
 
-get '/result' do
-	search_string = params[:movie]
-	search = Typhoeus.get("www.omdbapi.com/", :params => {:s => search_string})
-	result = JSON.parse(search.body)
+get '/show' do
+	search = Typhoeus.get("www.omdbapi.com/", :params => {:s => params[:movie]})
+	@result = JSON.parse(search.body)["Search"]
 		# puts result["Search"]
-		 @title = instance["Title"] 
-		 @year = instance["Year"]
-		 @id = instance["imdbID"]
-		 # search = Typhoeus.get("www.omdbapi.com", :params => {:i => id})
-		 # result2 = JSON.parse(search.body)
-		 # @poster = result2["Poster"]
-		 erb :show
+		# puts @result
+		 # @title = @result["Title"]
+		 # @year = @result["Year"]
+		 # @id = @result["imdbID"]
+		 # @rated = @result["Rated"]
+		 # @actors = @result["Actors"]
+		 # @plot = @result["Plot"]
+
+	# 	 search2 = Typhoeus.get("www.omdbapi.com", :params => {:i => id})
+	# 	 result2 = JSON.parse(search2.body)
+	# 	 @poster = result2["Poster"]
+	# @greeting = "Hello"
+	# @show = "Hello"
+	erb :show
+end
+
+get '/movie_info/:imdbID' do
+	search = Typhoeus.get("www.omdbapi.com/", :params => {:i => params["imdbID"]})
+	@poster = JSON.parse(search.body)
+	erb :movie_info
+	
+
+	# @year = info["Year"]
+	# @id = info["imdbID"]
+	# @rated = @result["Rated"]
+	# @actors = @result["Actors"]
+	# @plot = @result["Plot"]
+
 end
