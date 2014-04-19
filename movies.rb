fea9993017eee9846ee4375631cd654226d2a7b9
shane <shanemiller500@gmail.com>
2014-04-19 09:40:47 -0700

    updated

--- movies.rb
+++ movies.rb
@@ -32,45 +32,6 @@ get '/movie/:imdbID' do
   @pic = JSON.parse(res.body)
   # @temp = pic.inspect
   
-
-
 erb :show
 end
-
-
-
-
-# # A setup step to get rspec tests running.
-# configure do
-#   root = File.expand_path(File.dirname(__FILE__))
-#   set :views, File.join(root,'views')
-# end
-
-# get '/' do
-#   erb:search
-# end
-
-
-# get '/results' do
-  
-#   res=Typhoeus.get("www.omdbapi.com/", :params => { :s => params["movie"] }) 
-#   json = JSON.parse(res.body) 
-#   str = ""
-   
-  
-#   json["Search"].each do |movie|
-    
-#   # erb:index
-#   end
-  
-  
-
-# end
-
-# get '/poster/:imdbID' do
-#   res=Typhoeus.get("www.omdbapi.com/", :params => { :i => params["imdbID"] })
-#   pic = JSON.parse(res.body) 
-#   #"<img src = #{pic["Poster"]} />"
-# erb :show
-# end
 
