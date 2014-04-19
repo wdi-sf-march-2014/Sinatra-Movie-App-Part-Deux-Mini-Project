2f4a0efcdd976009c32a03c073a344fd3b1ccf7f
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-04-19 10:29:30 -0700

    foundation built

--- movies.rb
+++ movies.rb
@@ -1,16 +1,40 @@
+require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
-require 'pry'
-
-# A setup step to get rspec tests running.
-configure do
-  root = File.expand_path(File.dirname(__FILE__))
-  set :views, File.join(root,'views')
-end
 
 get '/' do
+
+  erb :index
 end
 
+get '/results' do
+  search = params[:movie]
+  if search == "" || search == nil
+    return "Movie not found"
+  end
 
+  results = Typhoeus.get("http://www.omdbapi.com/?s=#{search}")
+  omdb_data = JSON.parse(results.body)
+  omdb_data.inspect
+  @movies = omdb_data["Search"]
+
+  erb :search
+end
+
+get '/poster/:imdbID' do
+  @search = params[:imdbID]
+  results = Typhoeus.get("http://www.omdbapi.com/?i=#{@search}")
+  @picture = JSON.parse(results.body)
+
+  @poster = @picture["Poster"]
+  @plot = @picture["Plot"]
+  @release = @picture["Released"]
+  @rated = @picture["Rated"]
+  @runtime = @picture["Runtime"]
+  @genre = @picture["Genre"]
+  @director = @picture["Director"]
+  @actor = @picture["Actors"]
+  erb :poster
+end
--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -1,13 +1,24 @@
-require File.expand_path '../spec_helper.rb', __FILE__
+# require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "Sinatra Movie Application With OMDB API" do
+# describe "Sinatra Movie Application With OMDB API" do
+#   def app
+#     Sinatra::Application
+#   end
 
-  describe "Home page" do
-    it "should allow accessing the home page" do
-      get '/'
-      last_response.should be_ok
-    end
-  end
+# 	describe "Sinatra Movie Application With OMDB API" do
+# 	  describe "Home page" do
+# 	    it "should allow accessing the home page" do
+# 	      get '/'
+# 	      last_response.should be_ok
+# 	    end
+# 	  end
+	
+# 	describe "Search Results" do
+# 	    it "should return results for sharknado" do
+# 	      get '/results?movie=sharknado'
+# 	      last_response.should be_ok
+# 	    end
+	  
+# 	end
 
-  
-end
+# end
