d35795b22f38d31bc1ea7a9539cdf93fc46895dd
timsanty <timsanty@me.com>
2014-04-18 22:36:40 -0700

    phase 1 and 2

--- movies.rb
+++ movies.rb
@@ -10,7 +10,37 @@ configure do
   set :views, File.join(root,'views')
 end
 
-get '/' do
+ get '/' do
+	erb :search
 end
+
+get '/results' do
+	if params["movie"] == ""
+	redirect '/'
+	end
+
+	result = Typhoeus.get("http://www.omdbapi.com/", :params => { :s => params["movie"] })  # :y => params["year"]
+	response = JSON.parse(result.body)
+	str = ""
+
+	response["Search"].each do |movie|
+	str += "<br><a href=poster/#{movie["imdbID"]}> #{movie["Title"]} - #{movie["Year"]} </a><br>"""
+	end
+		str
+end
+#erb :show
+
+
+get '/poster/:imdbID' do
+	res = Typhoeus.get("http://www.omdbapi.com/", :params => { :i => params["imdbID"] })
+	pic = JSON.parse(res.body) 
+	#pic.inspect
+	"<img src = #{pic["Poster"]} />  #{pic["Plot"]} \n #{pic["Released Year"]}, #{pic["Rated"]}, #{pic["Runtime"]}, #{pic["Genre"]}, #{pic["Director"]}, #{pic["Actors"]}. "
+
+
+end
+
+
+
 
 
