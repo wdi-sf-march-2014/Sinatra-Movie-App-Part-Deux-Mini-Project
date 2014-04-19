0d23b62d389f941b1497a9c2e617337332ef4165
daveyk535 <dave@phase2minds.com>
2014-04-18 18:37:48 -0700

    4/18 basic ordered list with no links.

--- movies.rb
+++ movies.rb
@@ -8,14 +8,25 @@ require 'pry'
 configure do
   root = File.expand_path(File.dirname(__FILE__))
   set :views, File.join(root,'views')
-end
+	end
+
 
 get '/' do
-
-@greeting = "Hello"
-
-#designate the view
-erb :index
-end
+	erb :index
+	end
 
 
+get '/search' do
+	
+ 	search = Typhoeus.get("www.omdbapi.com", :params => {:s => params[:movie]})
+ 	result = JSON.parse(search.body)
+ 	@movies = result["Search"]
+	erb :Search
+	end
+
+
+
+
+# <%= search = Typhoeus.get("www.omdbapi.com", :params => {:i => id}) %>
+# <%= result2 = JSON.parse(search.body) %>
+# <%= poster = result2["Poster"] %>
