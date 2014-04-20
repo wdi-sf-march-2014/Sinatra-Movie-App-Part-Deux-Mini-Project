9ff00842f8bbeab9594662adfd7adbfc405cd29b
Jade <jade.applegate@gmail.com>
2014-04-19 21:57:07 -0700

    working with basic structure, needs style

--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -9,13 +9,6 @@ describe "Sinatra Movie Application With OMDB API" do
     end
   end
 
-  describe "Home page" do
-    it "should allow accessing the home page" do
-      get '/'
-      last_response.should be_ok
-    end
-  end
-
   describe "Search Results" do
     it "should return results for the jungle book" do
       get '/results?movie=the+jungle+book'
