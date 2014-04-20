b02d221be1a717195465b19de7c5817c7417e7e1
Jason <jasonckim10@gmail.com>
2014-04-19 19:16:22 -0700

    sinatraproject

--- movies.rb
+++ movies.rb
@@ -10,7 +10,33 @@ configure do
   set :views, File.join(root,'views')
 end
 
-get '/' do
+get "/" do
+
+erb :index
 end
+  
+get '/search' do
+ 	search_str = params[:movie]
+ 	response = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => search_str})
+ 	omdbdata = JSON.parse(response.body)
+ 	@movies = omdbdata["Search"]
+ erb :search
+ end
+ 
 
-
+get '/movies/:imdbID' do
+ 	imdb_id = params[:imdbID]
+ 	info = Typhoeus.get("http://www.omdbapi.com/", :params => {:i => imdb_id})
+ 	infodata = JSON.parse(info.body)
+ 	@plot = infodata["Plot"]
+ 	@year = infodata["Year"]
+ 	@rated = infodata["Rated"]
+ 	@runtime = infodata["Runtime"]
+ 	@genre = infodata["Genre"]
+ 	@director =infodata["Director"]
+ 	@actors = infodata["Actors"]
+ 	@poster = infodata["Poster"]
+ 	@title = infodata["Title"]
+ 	@link = infodata["imdbID"]
+ erb :show
+ end
