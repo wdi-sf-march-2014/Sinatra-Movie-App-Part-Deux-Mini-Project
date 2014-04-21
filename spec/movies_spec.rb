fcb7f76f9136e999779dc07830e84fd48bd491ce
ldrohan <drohanluke@gmail.com>
2014-04-20 19:54:40 -0700

    Final Final

--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -13,6 +13,20 @@ describe "Sinatra Movie Application With OMDB API" do
   		get '/results?movie=frozen'
   		last_response.should be_ok
    end		
+    it "should show a page when no search terms are entered" do
+      get '/results'
+      last_response.should be_ok
+    end  
+    it "should return frozen as a search result when it is searched for" do
+      get '/results?movie=frozen'
+      last_response.should be_ok
+      expect(last_response.body).to include("Frozen")
+    end
+    it "should return the year frozen was made" do
+      get '/results?movie=frozen'
+      last_response.should be_ok
+      expect(last_response.body).to include("2013")
+    end
   end
 
 
