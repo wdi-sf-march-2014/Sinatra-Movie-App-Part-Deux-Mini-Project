c96fa9f516e7b4a86661c5221eef2d23e93fc5d5
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-04-21 10:00:17 -0700

    My Movie Project

--- movies.rb
+++ movies.rb
@@ -4,13 +4,38 @@ require 'typhoeus'
 require 'json'
 require 'pry'
 
-# A setup step to get rspec tests running.
 configure do
   root = File.expand_path(File.dirname(__FILE__))
   set :views, File.join(root,'views')
 end
 
 get '/' do
+
+  erb :index
 end
 
+
+
+get '/search' do
+  if params["movie"] == ""
+    redirect '/search'
+  end
+  response = Typhoeus.get("http://www.omdbapi.com/",:params => {:s => params[:movie]})
+  @result1 = JSON.parse(response.body)
+  @movies = @result1["Search"]
+
+  erb :search
+
+end
+
+
+get '/show/:imdbID' do
+
+  response = Typhoeus.get("http://www.omdbapi.com/",:params => {:i => params["imdbID"]})
+  @result2 = JSON.parse(response.body)
+  @picture = @result2["Poster"]
+  
+erb :show
+
+end
 
