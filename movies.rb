81e56d99627f4b3d99d94b151d2e938bce585c89
petervargaofficial <petervarga@Peters-MacBook-Pro.local>
2014-04-21 00:17:26 -0700

    css needed

--- movies.rb
+++ movies.rb
@@ -10,7 +10,24 @@ configure do
   set :views, File.join(root,'views')
 end
 
+
 get '/' do
+   erb :index
 end
 
+get '/results' do
+	@movies = JSON.parse(Typhoeus.get("http://www.omdbapi.com/?s=" + params[:movie]).body)["Search"]
+	@movies = @movies.sort_by {|movie| movie["Year"].to_i}
+	if @movies == nil
+		erb :index
+	# TODO: for multi words strings that doesn't exist the code breaks e.g. "lasdf asd"
+	# make comment about movie non-existence before returning to Homepage		
+	else
+		erb :search
+	end
+end
 
+get '/poster/:imdbID' do
+	@posterDetail = JSON.parse(Typhoeus.get("http://www.omdbapi.com/?i=" + params[:imdbID]).body)
+	erb :show
+end
