bc6332afcc2d8ac401516d07746574ef03bdc6e7
Henry Huang <hhuang2088@gmail.com>
2014-04-21 00:02:47 -0700

    initial finish

--- app.rb
+++ app.rb
@@ -5,41 +5,20 @@ require 'json'
 require 'pry'
 
 get '/' do
-	@method = "get"
-	@action = "/show"
 	erb :index
 end
 
 get '/show' do
 	search = Typhoeus.get("www.omdbapi.com/", :params => {:s => params[:movie]})
 	@result = JSON.parse(search.body)["Search"]
-		# puts result["Search"]
-		# puts @result
-		 # @title = @result["Title"]
-		 # @year = @result["Year"]
-		 # @id = @result["imdbID"]
-		 # @rated = @result["Rated"]
-		 # @actors = @result["Actors"]
-		 # @plot = @result["Plot"]
-
-	# 	 search2 = Typhoeus.get("www.omdbapi.com", :params => {:i => id})
-	# 	 result2 = JSON.parse(search2.body)
-	# 	 @poster = result2["Poster"]
-	# @greeting = "Hello"
-	# @show = "Hello"
 	erb :show
 end
 
 get '/movie_info/:imdbID' do
 	search = Typhoeus.get("www.omdbapi.com/", :params => {:i => params["imdbID"]})
-	@poster = JSON.parse(search.body)
+	@result2 = JSON.parse(search.body)
+	# trailer = Typhoeus.get("http://api.traileraddict.com/")
+	# p trailer
+	# @trailer = Json.parse(trailer.body)
 	erb :movie_info
-	
-
-	# @year = info["Year"]
-	# @id = info["imdbID"]
-	# @rated = @result["Rated"]
-	# @actors = @result["Actors"]
-	# @plot = @result["Plot"]
-
 end
