185f0b8876c01e6f14abd5fb29172bb2cf96788a
Stuart_Grossman <grossman.stuart1@gmail.com>
2014-04-21 08:29:05 -0700

    done

--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -1,13 +1,39 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "Sinatra Movie Application With OMDB API" do
+describe 'Sinatra Movie Application With OMDB API' do
 
-  describe "Home page" do
-    it "should allow accessing the home page" do
+  describe 'Home page' do
+    it "accessing the home page" do
       get '/'
       last_response.should be_ok
     end
   end
 
-  
+  describe 'Results page' do
+    it "should return results" do
+      get '/results?movie=say+anything'
+      last_response.should be_ok
+    end
+
+    it "should return Say Anything" do
+      get '/results?movie=say+anything'
+      last_response.should be_ok
+      expect(last_response.body).to include("Say Anything...")
+    end
+end
+
+
+  describe 'Error page' do
+    it "return error page when the API doesn't return any results" do
+      get '/results?movie=too+specific'
+      last_response.should be_ok
+    end
+
+    it "should include an apology for not returning anything" do
+      get '/results'
+      last_response.should be_ok
+      expect(last_response.body).to include("Sorry")
+    end
+  end
+
 end
